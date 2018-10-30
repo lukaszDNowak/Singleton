df206130867108414323d731aabd5e21c5dfb2f6
Lukasz Nowak <Lukasz.d.nowak@interia.pl>
2018-10-31 00:45:06 +0100

    add WorkingTime()

--- Singleton1/Program.cs
+++ Singleton1/Program.cs
@@ -11,23 +11,37 @@ namespace Singleton1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(RomanEmployee.IsWorking());
-            RomanEmployee.Start();
-            Console.WriteLine(RomanEmployee.IsWorking());
+            Console.WriteLine("Roman rozpoczyna pracê");
+            var roman = RomanEmployee.Start();
+            Console.WriteLine($"Pracownik jest w pracy: {RomanEmployee.IsWorking()}");
+            Console.WriteLine("Roman koñczy pracê");
             RomanEmployee.Stop();
-            Console.WriteLine(RomanEmployee.IsWorking());
-            Console.ReadKey();
+            Console.WriteLine($"Pracownik jest w pracy: {RomanEmployee.IsWorking()}");
+
+            Console.WriteLine("Roman nie pracuje");
+            Console.WriteLine($"Pracownik pracuje ju¿ { RomanEmployee.WorkingTime()} ");
+            Console.WriteLine("Roman pracuje");
+            roman = RomanEmployee.Start();
+            Thread.Sleep(3000); // Czeka 3 sekundy.
+            Console.WriteLine($"Pracownik pracuje ju¿ { RomanEmployee.WorkingTime()}");
+            Console.ReadKey();
         }
     }
 
     public class RomanEmployee
     {
+        private static DateTime timeStart = DateTime.Now;
         private static RomanEmployee instance;
-        protected RomanEmployee() {}
+        protected RomanEmployee() { timeStart = DateTime.Now; }
         public static RomanEmployee Start() => instance == null ? instance = new RomanEmployee() : instance;
         public static bool IsWorking() => instance != null ? true : false;
         public static RomanEmployee Stop() =>  instance = null;
-   
+
+        internal static string WorkingTime()
+        {
+            string result = "-1";
+            return result = instance == null ? result : (DateTime.Now - timeStart).ToString("mmss");
+        }
     }
 
 }
